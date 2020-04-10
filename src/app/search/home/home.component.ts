09d8258eff490bf08a95fe8c7061ecfe3f267686
Iván Alejandro Díaz Acevedo <ivandiaz1919@gmail.com>
2020-04-10 18:14:51 -0500

    agregando mensajes de errores en el formulario y opciones avanzadas dinamicas

--- src/app/search/home/home.component.ts
+++ src/app/search/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,22 +8,44 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  searchForm: FormGroup;
-  areAdvancedOptionsDisplayed: boolean = false;
+  public searchForm: FormGroup;
+  public areAdvancedOptionsDisplayed: boolean = false;
+  public OPTIONS_LIMIT_NUMBER:number = 7;
+  public categories: string[] = ["Error Text", "Tag"];
+  public ERROR_TEXT_CATEGORY = this.categories[0];
+  public TAG_CATEGORY = this.categories[1];
+
+  
   constructor(private fb: FormBuilder, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.searchForm = this.fb.group({
-      searchWords: ['', Validators.required]
+      searchWords: ['', Validators.required],
+      advancedOptions: new FormArray([this.getNewAdvancedOption()])
     });
   }
 
-  showAdvancedOptions(){
-    this.areAdvancedOptionsDisplayed = !this.areAdvancedOptionsDisplayed;
+  private getNewAdvancedOption() : FormGroup{
+    return this.fb.group({
+      category: [this.categories[0], Validators.required],
+      term: ['', [Validators.required]]
+    });
   }
+
+  get controls() { return this.searchForm.controls; }
+  get advancedOptions() { return this.controls.advancedOptions as FormArray; }
+  get searchWords() { return this.searchForm.get('searchWords'); }
 
   ngOnDestroy() {
     //this.sub.unsubscribe();
+  }
+
+  public addSearchInput(){
+    this.advancedOptions.push(this.getNewAdvancedOption());
+  }
+
+  public removeSearchInput(index:number){
+    this.advancedOptions.removeAt(index);
   }
 
   public onResize(event:any) {
@@ -32,6 +54,7 @@ export class HomeComponent implements OnInit {
 
   public searchAnswers(){
     console.log(this.searchForm.get("searchWords").value);
+    console.log(this.searchForm.get("advancedOptions").value);
   }
 
 }
